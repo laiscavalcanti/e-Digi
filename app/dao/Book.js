ebd758cdc3178e0d66da6564eb9eb826726647dd
laiscavalcanti <cavalcantislais@gmail.com>
2020-08-06 01:18:33 -0300

    mudança nos nomes dos metodos de book

--- app/dao/Book.js
+++ app/dao/Book.js
@@ -5,11 +5,11 @@ export default class BookDAO {
   constructor() {
     this._list = [];
   }
-  title(book) {
+  hasTitle(book) {
     return this._list.some((b) => b.title === book.title);
   }
-  isbn(book) {
-    return this._list.some((i) => i.isbn === book.isbn);
+  hasIsbn(book) {
+    return this._list.some((b) => b.isbn === book.isbn);
   }
 
   validationBook(book) {
@@ -18,12 +18,12 @@ export default class BookDAO {
     if (!(book instanceof Book)) {
       throw new Error(`O Objeto não é do tipo Livro`);
     }
-    if (this.title(book)) {
+    if (this.hasTitle(book)) {
       validate.addError(
         new Error(`Já existe um cadastro de livro com esse título!`)
       );
     }
-    if (this.isbn(book)) {
+    if (this.hasIsbn(book)) {
       validate.addError(
         new Error(`Já existe um livro cadastrado com esse ISBN!`)
       );
